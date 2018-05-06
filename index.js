a3b5fb5ca4c8e82be3e9051f158ad704e9b6641c
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-06 17:20:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-const app = "I don't do much."
+const app = "I don't do much.";
+function beforeEach(){
+  kittens = ['Milo', 'Otis', 'Garfield'];
+}
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten(name){
+  var newArr = [...kittens, name];
+  return newArr;
+}
+
+function prependKitten(name){
+  var newArr = [name, ...kittens];
+  return newArr;
+}
+
+function removeLastKitten(name){
+  var newArr = [name, ...kittens];
+  return newArr;
+}
+
+function removeFirstKitten(name){
+  var newArr = [name, ...kittens];
+  return newArr;
+}
